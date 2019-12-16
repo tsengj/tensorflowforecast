44ef67d15f069433ddf5873f221e43d251e361e7
James Tseng <james.tseng00@gmail.com>
2019-12-16 23:46:46 +1100

    minor update

--- rscripts/install_tensor.R
+++ rscripts/install_tensor.R
@@ -1,11 +1,11 @@
 #https://github.com/rstudio/keras/issues/649
 devtools::install_github("rstudio/tensorflow")
 devtools::install_github("rstudio/keras")
-tensorflow::install_tensorflow(method = "auto",version = "gpu") #"gpu" 'cpu'
+tensorflow::install_tensorflow(method = "auto",version = "default") #"gpu" 'default'
 
 library(keras)
 install_keras(method = 'auto',
-              conda = "auto", version = "default", tensorflow = "gpu") #"gpu" 'cpu'
+              conda = "auto", version = "default", tensorflow = "default") #"gpu" 'default'
 
 # To activate this environment, use
 # conda activate r-reticulate
